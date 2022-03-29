183bc4273010a7807780affd0670c31f2cabc45b
Setareh Nazeri <s.nazeri@khallagh.com>
2022-03-29 11:01:51 +0430

    add check_distro function

--- checkports.sh
+++ checkports.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
-install_pack() {
-        dpkg -l nmap 1> /dev/null
+check_distro() {
+	distro_name=$(cat /etc/*elease | grep -w NAME | awk -F '=' '{print $2}')
+	echo -e "\033[0;33mLinux distribution name: $distro_name ... \033[0m"
+}
+install_pack_ubuntu() {
+	dpkg -l nmap 1> /dev/null
         if [[ "$?" != "0" ]]; then
                 echo -e "\033[0;33mNetcat is NOT installed ... \033[0m"
                 sudo apt update
-                sudo apt install nmap
+                sudo apt -y install nmap
         else
                 echo -e "\033[0;32mNetcat Is Installed ... \033[0m"
         fi
 }
+install_pack_centos() {
+        rpm -qa | grep -i nmap 1> /dev/null
+        if [[ "$?" != "0" ]]; then
+                echo -e "\033[0;31mNetcat is NOT installed ... \033[0m"
+                sudo yum -y install nmap
+        else
+                echo -e "\033[0;32mNetcat Is Installed ... \033[0m"
+        fi
+}
+
 get_IP() {
         read -p "Please Enter the Destination IP/Host:(ex:127.0.0.1) " IP_DEST
         get_port=$(nmap -sT -p- $IP_DEST | grep -wo ^[0-9]*)
@@ -22,5 +36,13 @@ get_IP() {
 
 }
 
-install_pack
-get_IP
+
+check_distro
+if [ $(echo $distro_name | grep -i ubuntu1) ]; then
+	install_pack_ubuntu
+elif [ `echo $distro_name | grep -i centos` ]; then
+	install_pack_centos
+else
+	echo -e "\033[0;32mLinux distribution name is NOT CENTOS or UBUNTU ... \033[0m"
+fi
+#get_IP
